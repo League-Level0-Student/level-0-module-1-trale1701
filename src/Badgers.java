8054eb6af6c242eafc8c60bcc7462819599bf549
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-07 13:23:42 -0700

    Robot in space, My ages, Badgers ip

---
+++ src/Badgers.java
@@ -0,0 +1,17 @@
+//Badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
+//Mushroom, mushroom, a
+//Badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
+//Mushroom, mushroom, a
+//Badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
+//Mush-mushroom, a
+//Badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
+//African! Snake, a snake
+//Snake! A snake, ooh it's a snake
+//It's a badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
+//Mushroom, mushroom, a
+public class Badgers {
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/MyAges.java
@@ -0,0 +1,8 @@
+
+public class MyAges {
+	public static void main(String[] args) {
+		for (int age = 0; age < 15; age++) {
+			System.out.println("your age is "+age);
+		}
+	}
+}
